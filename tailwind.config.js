6935195009b9d4dddb1d6f660bba517cc6dd3433
FelipeOropeza <felipe2006.co@gmail.com>
2024-11-11 21:09:05 -0300

    build: instalação do tailwind no projeto

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./app/views/**/*.{html,ejs,js}", // Certifique-se de incluir os arquivos onde você vai usar o Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
