acb6c43400fcc9c2803b465ed5be9d5318a05580
FelipeOropeza <felipe2006.co@gmail.com>
2024-11-17 23:55:54 -0300

    feat: imprementação do login e cadastro da empresa e uso de sessao

--- app/app.js
+++ app/app.js
@@ -1,13 +1,31 @@
 import cors from "cors";
 import express from "express";
 import routes from "./routes/index.js";
+import session from "express-session";
 
 const app = express();
-app.set('view engine', 'ejs');
-app.set('views', 'app/views');
-app.use(express.static('./public'));
+app.set("view engine", "ejs");
+app.set("views", "app/views");
+app.use(express.static("./public"));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "seuSegredoAqui",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.dados = req.session.dados || null; 
+  next();
+});
+
 app.use(cors());
 routes(app);
 
--- app/controller/empresaController.js
+++ app/controller/empresaController.js
@@ -1,9 +1,65 @@
-import { insertEmpresa } from "../service/empresaService.js";
+import { getbyEmpresa, insertEmpresa } from "../service/empresaService.js";
+import { verificarSenha } from "../utils/hashUtil.js";
 
 class EmpresaController {
   static loginEmpresa(req, res) {
-    res.render("index");
+    res.render("index", { errorMessage: "", successMessage: "" });
   }
+
+  static async postLoginEmpresa(req, res) {
+    try {
+      const { email, senha } = req.body;
+  
+      if (!email || !senha) {
+        return res.render("index", {
+          errorMessage: "Todos os campos são obrigatórios.",
+          successMessage: "",
+          email: "",
+          senha: ""
+        });
+      }
+  
+      const empresa = await getbyEmpresa([{ email }]);
+      if (!empresa) {
+        return res.render("index", {
+          errorMessage: "Empresa não encontrada.",
+          successMessage: "",
+          email: "",
+          senha: ""
+        });
+      }
+  
+      const bool = await verificarSenha(senha, empresa.passwordEmpresa);
+      if (!bool) {
+        return res.render("index", {
+          errorMessage: "A senha está incorreta.",
+          successMessage: "",
+          email: "",
+          senha: "" 
+        });
+      }
+  
+      const { passwordEmpresa, cnpj, ...empresaSemDadosSensíveis } = empresa;
+  
+      req.session.dados = { empresa: empresaSemDadosSensíveis };
+  
+      return res.render("index", {
+        errorMessage: "",
+        successMessage: "Login feito com sucesso!",
+        email: "",
+        senha: ""
+      });
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      return res.render("index", {
+        errorMessage: "Erro interno do servidor.",
+        successMessage: "",
+        email: "",
+        senha: ""
+      });
+    }
+  }
+  
 
   static cadastroEmpresa(req, res) {
     res.render("formEmpresa", { errorMessage: "", successMessage: "" });
@@ -11,25 +67,28 @@ class EmpresaController {
 
   static async postEmpresa(req, res) {
     try {
-        const { nmEmpresa, cnpj, emailEmpresa, passwordEmpresa } = req.body;
+      const { nmEmpresa, cnpj, emailEmpresa, passwordEmpresa } = req.body;
 
-        if (!nmEmpresa || !cnpj || !emailEmpresa || !passwordEmpresa) {
-            return res.render('formEmpresa', { errorMessage: "Todos os campos são obrigatórios.", successMessage: "" });
-        }
-        // const cnpjRegex = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/;
-        // if (!cnpjRegex.test(cnpj)) {
-        //     return res.render('formEmpresa', { errorMessage: "CNPJ em formato inválido.", successMessage: "" });
-        // }
+      if (!nmEmpresa || !cnpj || !emailEmpresa || !passwordEmpresa) {
+        return res.render("formEmpresa", {
+          errorMessage: "Todos os campos são obrigatórios.",
+          successMessage: "",
+        });
+      }
 
-        await insertEmpresa([{nmEmpresa, cnpj, emailEmpresa, passwordEmpresa}]);
-        res.render('formEmpresa', { errorMessage: "", successMessage: "Empresa cadastrada com sucesso!" });
+      await insertEmpresa([{ nmEmpresa, cnpj, emailEmpresa, passwordEmpresa }]);
+      res.render("formEmpresa", {
+        errorMessage: "",
+        successMessage: "Empresa cadastrada com sucesso!",
+      });
     } catch (error) {
-        console.error("Erro ao cadastrar empresa:", error);
-        res.render('formEmpresa', { errorMessage: "Erro interno do servidor.", successMessage: "" });
+      console.error("Erro ao cadastrar empresa:", error);
+      res.render("formEmpresa", {
+        errorMessage: "Erro interno do servidor.",
+        successMessage: "",
+      });
     }
-}
-
-
+  }
 }
 
 export default EmpresaController;
