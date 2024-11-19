3e475be99c5defe59708360e629cb07a9617b198
FelipeOropeza <felipe2006.co@gmail.com>
2024-11-19 00:32:26 -0300

    feat: criação do navbar pra melhor visualização do sistema, criarção de session pra armazenar o guiche que o sistema vai ser chamado e implemntação das rotas logout e set-guiche

--- app/controller/homeController.js
+++ app/controller/homeController.js
@@ -1,6 +1,29 @@
-class HomeController{
-    static home(req, res){
-        res.render('home');
+class HomeController {
+    static home(req, res) {
+        if (!req.session.dados) {
+            return res.redirect('/');
+        }
+    
+        const guiche = req.session.guiche || null;
+            res.render('home', { guiche });
+    }
+    
+
+    static setGuiche(req, res) {
+        const { guiche } = req.body;
+        req.session.guiche = guiche;
+        res.redirect('/home');
+    }
+
+    static logout(req, res) {
+        req.session.destroy(err => {
+            if (err) {
+                console.error("Erro ao destruir a sessão:", err);
+                return res.status(500).send("Erro ao realizar logout.");
+            }
+            res.clearCookie('connect.sid');
+            res.redirect('/');
+        });
     }
 }
 
--- app/routes/homeRoute.js
+++ app/routes/homeRoute.js
@@ -4,5 +4,7 @@ import homeController from "../controller/homeController.js";
 const routes = express.Router();
 
 routes.get("/home", homeController.home);
+routes.post("/set-guiche", homeController.setGuiche);
+routes.get("/logout", homeController.logout);
 
 export default routes;
